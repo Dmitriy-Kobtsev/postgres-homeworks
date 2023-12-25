f0a05453e01000281909d157b6617b5890aebb5c
Dmitriy-Kobtsev <kibtsev94@gmail.com>
2023-12-25 23:39:30 +0300

    add sql in simple_queries.sql

--- homework-2/simple_queries.sql
+++ homework-2/simple_queries.sql
@@ -1,9 +1,10 @@
 -- Напишите запросы, которые выводят следующую информацию:
 -- 1. "имя контакта" и "город" (contact_name, city) из таблицы customers (только эти две колонки)
-SELECT ...
+SELECT contact_name, city FROM customers
 
 -- 2. идентификатор заказа и разницу между датами формирования (order_date) заказа и его отгрузкой (shipped_date) из таблицы orders
-
+SELECT order_id, abs(order_date - shipped_date) as days_diff
+FROM orders;
 
 -- 3. все города без повторов, в которых зарегистрированы заказчики (customers)
 
